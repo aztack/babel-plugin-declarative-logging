47e1522d0c6adfc4aed0d29f6341cc227f373015
wangweihua <aztack@163.com>
2019-02-12 14:44:13 +0800

    bigbang

---
+++ declarative-logging.js
@@ -0,0 +1,47 @@
+const DefaultMapping = {
+  "__log": "log",
+  "__warn": "warn",
+  "__error": "error"
+};
+
+const DecorativeIds = ["_","__","___","____"];
+
+module.exports = function(o) {
+  const t = o.types;
+  return {
+    visitor: {
+      LabeledStatement (path, state) {
+        if (!state.opts || typeof state.opts !== 'object') return;
+        if (!Object.keys(state.opts).length) return;
+
+        // only handle labels inside a class method
+        if (!path.parentPath) return;
+        if (!path.parentPath.parent) return;
+
+        const parentType = path.parentPath.parent.type;
+        if (parentType !== 'ClassMethod' && parentType != 'ObjectMethod') return;
+
+        const mapping = state.opts.mapping || DefaultMapping;
+        // skip unsupported lables
+        const name = path.node.label.name;
+        if(!mapping[name]) return;
+
+        const type = path.node.body.expression.type;
+        let args = [];
+        if (type === 'SequenceExpression') {
+          args = path.node.body.expression.expressions;
+          // remove last decorative identifiers
+          let idName = args[args.length-1].name
+          if ( DecorativeIds.indexOf(idName) >= 0) args.pop();
+        } else {
+          args = [path.node.body.expression]
+        }
+
+        // replace labels with method call
+        const method = mapping[name];
+        const methodCall = t.CallExpression(t.MemberExpression(t.ThisExpression(), t.Identifier(method)), args);
+        path.replaceWith(methodCall);
+      }
+    }
+  }
+};
---
+++ test.js
@@ -0,0 +1,33 @@
+/**
+ * labels inside functions won't be transformed
+ */
+function f () {
+  log: "hehe", 13, Math.PI
+}
+
+/**
+ * only labels inside class method and object method wil be transformed into method call
+ */
+
+class Log {
+  log (...args) {
+    console.log(...args)
+  }
+}
+
+class A extends Log{
+  m () {
+    // last decorative underscore will be removed
+    __log: 1, 2, __
+  }
+}
+
+const B = {
+  m () {
+    // object literal should be wrapped with parenthesis
+    __warn: ({name:"a"}), __;
+  },
+  __warn (...args) {
+    console.warn(...args);
+  }
+}
